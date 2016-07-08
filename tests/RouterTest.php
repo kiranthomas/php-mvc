c19afce6fc8f51530dc915eaba1dc043196a6f57
Kiran Thomas <kiranthomasrose@gmail.com>
2016-07-08 11:41:00 +0100

    Tests Update

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -10,16 +10,37 @@ class RouterTest extends PHPUnit_Framework_TestCase
         $r = new Router();
     }
 
-    public function testGetRoutes()
+    public function testRoutingTableIsArray()
     {
         $router = new Router();
+        $router->add('');
 
-        $router->add('{controller}/{action}');
-
+        $this->assertInternalType('array',$router->getRoutes());
 
     }
 
+    Public function testRoutingTableHasKey()
+    {
+        $router = new Router();
+        $router->add('');
+        $router->add('{controller}');
+        $router->add('{controller}/{action}');
+        $router->add('{controller}/{id:\d+}/{action}');
 
+        $this->assertArrayHasKey('/^$/i', $router->getRoutes());
+        $this->assertArrayHasKey('/^(?P<controller>[a-z-]+)$/i', $router->getRoutes());
+        $this->assertArrayHasKey('/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/i', $router->getRoutes());
+        $this->assertArrayHasKey('/^(?P<controller>[a-z-]+)\/(?P<id>\d+)\/(?P<action>[a-z-]+)$/i', $router->getRoutes());
+
+    }
+
+    public function testRoutingTableHasValue()
+    {
+        $router = new Router();
+        $router->add('', 'User');
+
+        $this->assertContains('User', $router->getRoutes());
+    }
 
 
 }
